72372e0cf13facf1da123ef9c5390867eb149770
Gautam Raj <grjdeveloper@gmail.com>
2022-04-16 22:05:27 +0530

    updated

--- OutSourceDSAquestions/src/main/java/pramp/ArrayOfArrayProducts.java
+++ OutSourceDSAquestions/src/main/java/pramp/ArrayOfArrayProducts.java
@@ -3,7 +3,7 @@ package pramp;
 import java.util.Arrays;
 
 /**
- * @author gautamarj - ssdbad
+ * @author gautamraj - ssdbad
  * Given an array of integers arr, you’re asked to calculate for each index i
  * the product of all integers except the integer at that index (i.e. except arr[i]).
  * Implement a function arrayOfArrayProducts that takes an array of integers and
@@ -22,6 +22,7 @@ public class ArrayOfArrayProducts {
     private static int[] findArrayOfArrayProducts(int[] array) {
         /**
          * Finding left product array with left-most index value 1
+         * [1, 2, 14, 42]
          */
         int leftArray[] = new int[array.length];
         int product = 1;
@@ -33,6 +34,7 @@ public class ArrayOfArrayProducts {
 
         /**
          * Finding right product array with right-most index value 1
+         * [84, 12, 4, 1]
          */
         int rightArray[] = new int[array.length];
         product = 1;
@@ -44,6 +46,7 @@ public class ArrayOfArrayProducts {
 
         /**
          * At the time of output, we can use both array for multiplication
+         * [84, 24, 56, 42]
          */
         int outputArray[] = new int[array.length];
         for (int index = 0 ; index < array.length ; index++) {
@@ -58,4 +61,6 @@ public class ArrayOfArrayProducts {
 Input       :     Output        :   Hint
 {8,10,2}    :     {20,16,80}    :   # by calculating = [10*2, 8*2, 8*10]
 {2,7,3,4}   :     {84,24,56,42} :   # by calculating = [7*3*4, 2*3*4, 2*7*4, 2*7*3]
+
+Reference   :   https://www.geeksforgeeks.org/a-product-array-puzzle/
 */
