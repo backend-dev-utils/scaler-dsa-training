af606cba2e7b783ee364270874223e5ae5d47e60
Gautam Raj <grjdeveloper@gmail.com>
2022-04-16 21:46:18 +0530

    2D-Arrays

--- Intermediate-DSA-Class/src/main/java/intermediate/homework/lecture_24/AreMatricesSame.java
+++ Intermediate-DSA-Class/src/main/java/intermediate/homework/lecture_24/AreMatricesSame.java
@@ -1,12 +1,14 @@
 package intermediate.homework.lecture_24;
 
+import java.util.Arrays;
+
 public class AreMatricesSame {
     public static void main(String[] args) {
         int firstArray[][] = {{1,2,3},{4,5,6},{7,8,9}};
         int secondArray[][] = {{1,2,3},{4,5,6},{7,8,9}};
 
-        int isSameMatrices = matricesAreSame(firstArray, secondArray);
-        System.out.println("Both Matrices are : "+isSameMatrices);
+        System.out.println("Both Matrices are : "+matricesAreSame(firstArray, secondArray));
+        System.out.println("Both Matrices are : "+areMatricesSame(firstArray, secondArray));
     }
 
     private static int matricesAreSame(int[][] firstArray, int[][] secondArray) {
@@ -15,6 +17,14 @@ public class AreMatricesSame {
                 if(firstArray[rows][columns] != secondArray[rows][columns])
                     return 0;
             }
+        }
+        return 1;
+    }
+
+    private static int areMatricesSame(int[][] firstArray, int[][] secondArray) {
+        for (int rows = 0; rows < firstArray.length; rows++) {
+            if(! Arrays.toString(firstArray[rows]).equals(Arrays.toString(secondArray[rows])))
+                return 0;
         }
         return 1;
     }
